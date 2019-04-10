64bad30ef0641d4bb446e57b3f6abb9590b1776f
alexandrafren <alexandra.fren@gmail.com>
2019-04-10 13:50:01 -0700

    exercise 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Alexandra Fren'
+my_age = 26
+my_height = 64
+my_weight = 160
+my_eyes = "Brown"
+my_teeth = "White"
+my_hair = "Black"
+
+puts "Let's talk about #{my_name}"
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth}."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
